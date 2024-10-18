ff423b2783bfc224623ea83ccf50195bfec95fc6
luckfer7 <lucasferreira059@gmail.com>
2024-10-18 11:13:44 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,66 @@
+let amigos = [];
+
+function adicionar () {
+    let nomeDoAmigo = document.getElementById('nome-amigo').value;
+    if (nomeDoAmigo == '') {
+        alert('Informe o nome do amigo');
+        return;
+    }
+
+    if (amigos.includes(nomeDoAmigo)) {
+        alert('Nome ja adicionado');
+        return;
+    }
+
+    let listaDeAmigos = document.getElementById('lista-amigos');
+
+    amigos.push(nomeDoAmigo);
+    
+    if (listaDeAmigos.textContent == '') {
+        listaDeAmigos.textContent = nomeDoAmigo;
+    } else {
+        listaDeAmigos.textContent = listaDeAmigos.textContent + ', ' + nomeDoAmigo;
+    }
+    
+    document.getElementById('nome-amigo').value = '';
+}
+
+function sortear() {
+
+    if (amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos');
+        return;
+    }
+
+    embaralha(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+
+        if (i == amigos.length -1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + '-->' + amigos[i + 1] + '<br>';
+        }
+        
+    }
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = ''; 
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
